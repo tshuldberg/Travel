a24c15a5b3386b26cfef17a2da5e14ac85099730
Trey Shuldberg <trey@Treys-MacBook-Pro.local>
2020-03-09 14:34:21 -0700

    readMe and Protected Routes fix

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const postsCtrl = require('../controllers/posts')
 
-router.get('/users/:id/posts/new', postsCtrl.new)
-router.post('/users/:id/posts', postsCtrl.create)
-router.get('/users/:id/posts', postsCtrl.showAll)
-router.get('/users/:id/posts/:postId', postsCtrl.showOne)
-router.put('/users/:id/posts/:postId', postsCtrl.update)
-router.delete('/users/:id/posts/:postId', postsCtrl.delete)
+router.get('/users/:id/posts/new', isLoggedIn, postsCtrl.new)
+router.post('/users/:id/posts', isLoggedIn, postsCtrl.create)
+router.get('/users/:id/posts', isLoggedIn, postsCtrl.showAll)
+router.get('/users/:id/posts/:postId', isLoggedIn, postsCtrl.showOne)
+router.put('/users/:id/posts/:postId', isLoggedIn, postsCtrl.update)
+router.delete('/users/:id/posts/:postId', isLoggedIn, postsCtrl.delete)
+
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) return next()
+    res.redirect('/auth/google')
+}
 
 module.exports = router;
